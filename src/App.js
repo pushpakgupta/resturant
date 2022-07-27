0c2f1de169e27dd41c0d5706a980c251c918fcff
pushpakgupta <pushpak.gupta2@gmail.com>
2022-07-27 14:37:56 +0530

    First comment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Resturant from './Component/Basic/Resturant';
+import './Component/Basic/Style.css';
+
+
+const App = () => {
+
+  return (
+    <>      
+      <Resturant />
+    </>
+  )
+}
+
+export default App;
